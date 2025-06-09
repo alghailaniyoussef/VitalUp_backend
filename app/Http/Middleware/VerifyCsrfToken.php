d72e1eb49e9add2a3e9951c9be37e28c098a796f
alghailaniyoussef <63921184+alghailaniyoussef@users.noreply.github.com>
2025-06-09 04:29:34 +0200

    Override CSRF token cookie creation to force SameSite=None

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -30,5 +31,35 @@ class VerifyCsrfToken extends Middleware
         }
 
         return parent::inExceptArray($request);
+    }
+
+    /**
+     * Add the CSRF token to the response cookies.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Symfony\Component\HttpFoundation\Response  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function addCookieToResponse($request, $response)
+    {
+        $config = config('session');
+
+        if ($response instanceof \Illuminate\Http\Response) {
+            $response->headers->setCookie(
+                new Cookie(
+                    'XSRF-TOKEN',
+                    $request->session()->token(),
+                    $this->availableAt(60 * $config['lifetime']),
+                    $config['path'],
+                    $config['domain'],
+                    true, // secure
+                    false, // httpOnly
+                    false, // raw
+                    'none' // sameSite
+                )
+            );
+        }
+
+        return $response;
     }
 }
